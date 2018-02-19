af8615b704ec37d16c7351ac6a0374ee6117f01b
Yanko Bolanos <y@rem7.com>
2018-02-19 09:11:00 -0800

    enabled service

--- instance_update.sh
+++ instance_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-source <( curl "http://169.254.169.254/latest/user-data" 2>/dev/null ) 
+source <( curl "http://169.254.169.254/latest/user-data" 2>/dev/null )
  
 if [ "${GO_GIT_HASH}" != "" ]; then
   pushd $PROJECT_DIR >/dev/null 2>/dev/null
   su node -c "vendor/github.com/rem7/servercontrol/git_update_to_hash.sh ${GO_GIT_HASH}" >>/tmp/instance-update.log 2>&1
   popd >/dev/null 2>/dev/null
-  /usr/bin/sv restart $GO_PROJECT
+  ln -s /etc/sv/$GO_PROJECT /etc/service/$GO_PROJECT
 fi
 
 
